633dc86440c1db7d2d5d8ccbd3897aab77d0af4d
Viswanath kodali <143597577+viswa0028@users.noreply.github.com>
2025-04-29 16:02:16 +0530

    Add files via upload

--- Arrays/maxsumsubarray.java
+++ Arrays/maxsumsubarray.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class maxsumsubarray {
     public static void main(String[] args) {
         int[] arr = {1,2,-4,4,4};
@@ -7,16 +9,21 @@ public class maxsumsubarray {
     public static int maxsum(int[] arr){
         int res = 0;
         int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
+        int[] ar = new int[arr.length];
+        for (int i = 0; i < arr.length; i++){
             sum = sum + arr[i];
             if (sum > res) {
                 res = sum;
+                ar[i] = arr[i];
             }
             if (sum < res) {
                 sum = 0;
+                for(int j=0;j<arr.length;j++){
+                    ar[arr.length-j-1] =0;
+                }
             }
         }
-
+        System.out.println(Arrays.toString(ar));
         return res;
     }
 }
